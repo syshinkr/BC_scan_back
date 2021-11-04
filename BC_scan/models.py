ecc55cb4cd8ba4e28d2e1401c07d29381f8646f9
syshinkr <syshinkr228@gmail.com>
2021-11-04 23:35:30 +0900

    change models

--- BC_scan/models.py
+++ BC_scan/models.py
@@ -2,7 +2,6 @@ from django.db import models
 import uuid
 
 class BusinessCard(models.Model):
-    # Fields
     """
     id: uuid [PK]
     user: user who added this business card
@@ -14,9 +13,10 @@ class BusinessCard(models.Model):
     company_address: company address [nullable]
     company_tell: Company's Representative call number [nullable]
     com_extension_tell: Owner's call number In company [nullable]
+    created_at: Automatically set the field to now(datetime) when the object is first created
     """
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='id for PK to this Business Card')
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
     name = models.CharField(max_length=8, help_text='name')
     mobile_tell = models.CharField(max_length=20, help_text='mobile tell')
     email = models.EmailField(max_length=50, help_text='email', null=True)
@@ -25,6 +25,7 @@ class BusinessCard(models.Model):
     company_address = models.CharField(max_length=100, help_text='company address', null=True)
     company_tell = models.CharField(max_length=20, help_text='company tell', null=True)
     com_extension_tell = models.CharField(max_length=20, help_text='company extension tell', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     
     # Metadata
     class Meta:
@@ -43,50 +44,27 @@ class User(models.Model):
     """
     id: id [PK]
     email: user email [UK]
-    pw: password for normal login [nullable] 
     name: user name
+    access_token: access token to access to social (Only Google)
+    refresh_token: refresh token to get new access token
+    created_at: Automatically set the field to now(date) when the object is first created
     
-    If id is none, email will be PK. 
-    But email is too long to use as pk
+    If user don't have a id, email will be PK. 
+    But, email is too long to use as PK.
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='ID for PK to user')
     email = models.EmailField(max_length=50, help_text='email', unique=True)
-    password = models.BinaryField(primary_key=True, help_text='password for normal login', null=True)
     name = models.CharField(max_length=8, help_text="user name")
+    access_token = models.CharField(primary_key=True, help_text='access token to access to social')
+    refresh_token = models.CharField(help_text='refresh token to get new access token', null=True)
+    created_at = models.DateField(auto_now_add=True)
 
     class Meta:
-        ordering = ['name']
+        ordering = ['email']
+
+    # def get_absolute_url(self):
+    #     return reverse('user-detail-view', args=[str(self.id)])
 
     def __str__(self):
         return f"{self.name} {self.email}"
 
-class UserToken(models.Model):
-    """
-    user: user who own this token
-    access_token: access token to access to social
-    refresh_token: refresh token to get new access token
-    type: social that be used to login's type
-    """
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
-    access_token = models.CharField(primary_key=True, help_text='access token to access to social')
-    refresh_token = models.CharField(help_text='refresh token to get new access token', null=True)
-    
-    SOCIAL_TYPE = (
-        ("f", "facebook"),
-        ("g", "google"),
-        ("n", "naver"),
-        ("k", "kakao"),
-        ("i", "instagram"),
-    )
-
-    type = models.CharField(
-        max_length = 1,
-        choices=SOCIAL_TYPE,
-        help_text="Social Type",
-    )
-
-    class Meta:
-        ordering = ['access_token']
-
-    def __str__(self):
-        return f"{self.user} {self.type} {self.access_token} {self.refresh_token}"
